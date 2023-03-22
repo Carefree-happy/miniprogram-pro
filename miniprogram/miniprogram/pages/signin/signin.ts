3a9075948208addf8d82fe010fd73d265dfecf04
Carefree-happy <2507057523@qq.com>
2023-03-22 14:52:07 +0800

    feat: 标签跳转

--- miniprogram/miniprogram/pages/signin/signin.ts
+++ miniprogram/miniprogram/pages/signin/signin.ts
@@ -5,9 +5,36 @@ Page({
    * Page initial data
    */
   data: {
-
+    message: "我有一只拿破仑"
+  },
+  changeData: function(){
+    this.setData({ message: "拿破仑今天不吃饭"});
+  },
+  getUsername: function(){
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res.userInfo.nickName)
+        that.setData({name: res.userInfo.nickName})
+      },
+      fail: function (res) {
+        console.log("fail", res)
+      }
+    })
   },
 
+  usrInfo: function() {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res.userInfo.nickName)
+        that.setData({name: res.userInfo.nickName})
+      },
+      fail: function (res) {
+        console.log("fail", res)
+      }
+    })
+  },
   /**
    * Lifecycle function--Called when page load
    */
